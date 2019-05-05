4de758fecdd87625c948a222ed88c7c24ccd5b98
Sujan Budhathoki <sujanbudhathoki123@gmail.com>
2019-05-05 14:38:52 +0545

    counter component value assigned

--- src/Component/counter.js
+++ src/Component/counter.js
@@ -1,13 +1,17 @@
 import React, { Component, useState } from 'react';
+import Button from '@material-ui/core/Button';
 import {useSpring, animated } from 'react-spring';
 
-
+let value = 100;
 const Counter = (props) => {
     const [count, setCount] = useState(0);
 
     return (
         <div>
             <h2>This is counter : {count}</h2>
+            <Button variant = "contained" color = "secondary">
+                Click
+            </Button>
         </div>
     )
 };
